cbdb894a672c313e1d1d2443af8bea7d945d6a7c
louis.guichard-montguers <louis.guichard-montguers@etu.uca.fr>
2025-04-07 14:34:08 +0200

    Clean reinit

---
+++ VivaxGPS/Vivax/vivax.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO.Ports;
+
+class vivax
+{
+    static void Main(string[] args)
+    {
+        SerialPort vivaxPort = new SerialPort("/tmp/vivax1", 9600); 
+
+        vivaxPort.DataReceived += VivaxDataReceived; //lie l'event à la méthode
+
+        try
+        {
+            vivaxPort.Open();
+            Console.WriteLine("Connexion au Vivax ouverte. En attente de données...");
+            Console.WriteLine("Appuie sur une touche pour arrêter.");
+            Console.ReadKey();
+            vivaxPort.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erreur : " + ex.Message);
+        }
+    }
+
+    private static void VivaxDataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort sp = (SerialPort)sender;
+        string trame = sp.ReadLine(); 
+
+        Console.WriteLine("Trame reçue : " + trame);
+
+   
+        string[] valeurs = trame.Split(',');
+
+
+        if (valeurs.Length >= 12)
+        {
+         
+            string log = valeurs[0];      
+            string location = valeurs[1]; 
+            int coord1 = int.Parse(valeurs[2]); 
+            int profondeur = int.Parse(valeurs[3]); 
+
+            string ligne = $"LOG: {log}, Location: {location}, Coordonnée 1: {coord1}, Profondeur: {profondeur}";
+
+         
+            using (StreamWriter writer = new StreamWriter("trames.txt", true))
+            {
+                writer.WriteLine(ligne); 
+            }
+
+            
+            Console.WriteLine(ligne);
+        }
+        else
+        {
+            Console.WriteLine("Trame mal formée, nombre d'éléments incorrect.");
+        }
+    }
+}
---
+++ VivaxGPS/gps/gps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class gps
+{
+    static void Main(string[] args)
+    {
+        // Crée un écouteur TCP pour l'adresse IP et le port spécifiés
+        IPAddress ip = IPAddress.Parse("92.142.1.246");  // Remplace par l'IP du GPS si nécessaire
+        TcpListener server = new TcpListener(ip, 9001);  // Utilise le bon port
+
+        try
+        {
+            server.Start();
+            Console.WriteLine("Serveur démarré, en attente de connexion...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client connecté");
+
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Trame reçue : " + receivedData);
+
+                    // Traite la trame ici
+                    // Par exemple, tu peux enregistrer ces données dans un fichier
+                    System.IO.File.AppendAllText("donnees_gps_reseau.txt", receivedData + "\n");
+                }
+
+                client.Close();  // Ferme la connexion après réception des données
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erreur : " + ex.Message);
+        }
+    }
+}
